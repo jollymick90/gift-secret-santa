2b27fd0f81dd41e0d4a94cb8fb9216eddbaa0404
JollyMick <scarpa.michele.90@gmail.com>
2024-12-07 07:23:39 +0100

    convert all code in ts class

---
+++ gift-secret-santa/src/character.ts
@@ -0,0 +1,283 @@
+import {
+  Colors,
+  deg2Rad,
+} from './constant';
+import {
+  createBox,
+  createGroup,
+  sinusoid,
+} from './ui-utils';
+
+/**
+ * The player's character in the game.
+ */
+export class Character {
+
+	// Explicit binding of this even in changing contexts.
+
+	// Character defaults that don't change throughout the game.
+    skinColor: any;
+    hairColor: any;
+    shirtColor: any;
+    shortsColor: any;
+    jumpDuration: any;
+    jumpHeight: any;
+    face: any;
+    hair: any;
+    head: any;
+    torso: any;
+    leftLowerArm: any;
+    leftArm: any;
+    rightLowerArm: any;
+    rightArm: any;
+    leftLowerLeg: any;
+    leftLeg: any;
+    rightLowerLeg: any;
+    rightLeg: any;
+    element: any;
+    isJumping: boolean = false;
+    isSwitchingLeft: boolean = false;
+    isSwitchingRight: boolean = false;
+    currentLane: number = 0;
+    runningStartTime: number = 0;
+    pauseStartTime: number = 0;
+    stepFreq: number = 0;
+    queuedActions: string[] = [];
+    jumpStartTime: number = 0;
+
+    constructor() {
+        this.skinColor = Colors.brown;
+        this.hairColor = Colors.black;
+        this.shirtColor = Colors.yellow;
+        this.shortsColor = Colors.olive;
+        this.jumpDuration = 0.6;
+        this.jumpHeight = 2000;
+    }
+
+
+	/**
+	  * Builds the character in depth-first order. The parts of are 
+  	  * modelled by the following object hierarchy:
+	  *
+	  * - character (this.element)
+	  *    - head
+	  *       - face
+	  *       - hair
+	  *    - torso
+	  *    - leftArm
+	  *       - leftLowerArm
+	  *    - rightArm
+	  *       - rightLowerArm
+	  *    - leftLeg
+	  *       - rightLowerLeg
+	  *    - rightLeg
+	  *       - rightLowerLeg
+	  *
+	  * Also set up the starting values for evolving parameters throughout
+	  * the game.
+	  * 
+	  */
+	init() {
+
+		// Build the character.
+		this.face = createBox(100, 100, 60, this.skinColor, 0, 0, 0);
+		this.hair = createBox(105, 20, 65, this.hairColor, 0, 50, 0);
+		this.head = createGroup(0, 260, -25);
+		this.head.add(this.face);
+		this.head.add(this.hair);
+
+		this.torso = createBox(150, 190, 40, this.shirtColor, 0, 100, 0);
+
+		this.leftLowerArm = this.createLimb(20, 120, 30, this.skinColor, 0, -170, 0);
+		this.leftArm = this.createLimb(30, 140, 40, this.skinColor, -100, 190, -10);
+		this.leftArm.add(this.leftLowerArm);
+
+		this.rightLowerArm = this.createLimb(
+			20, 120, 30, this.skinColor, 0, -170, 0);
+		this.rightArm =this.createLimb(30, 140, 40, this.skinColor, 100, 190, -10);
+		this.rightArm.add(this.rightLowerArm);
+
+		this.leftLowerLeg = this.createLimb(40, 200, 40, this.skinColor, 0, -200, 0);
+		this.leftLeg =this.createLimb(50, 170, 50, this.shortsColor, -50, -10, 30);
+		this.leftLeg.add(this.leftLowerLeg);
+
+		this.rightLowerLeg = this.createLimb(
+			40, 200, 40, this.skinColor, 0, -200, 0);
+		this.rightLeg = this.createLimb(50, 170, 50, this.shortsColor, 50, -10, 30);
+		this.rightLeg.add(this.rightLowerLeg);
+
+		this.element = createGroup(0, 0, -4000);
+		this.element.add(this.head);
+		this.element.add(this.torso);
+		this.element.add(this.leftArm);
+		this.element.add(this.rightArm);
+		this.element.add(this.leftLeg);
+		this.element.add(this.rightLeg);
+
+		// Initialize the player's changing parameters.
+		this.isJumping = false;
+		this.isSwitchingLeft = false;
+		this.isSwitchingRight = false;
+		this.currentLane = 0;
+		this.runningStartTime = new Date().getTime() / 1000;
+		this.pauseStartTime = new Date().getTime() / 1000;
+		this.stepFreq = 2;
+		this.queuedActions = [];
+
+	}
+
+
+	/**
+	 * Creates and returns a limb with an axis of rotation at the top.
+	 *
+	 * @param {number} DX The width of the limb.
+	 * @param {number} DY The length of the limb.
+	 * @param {number} DZ The depth of the limb.
+	 * @param {color} COLOR The color of the limb.
+	 * @param {number} X The x-coordinate of the rotation center.
+	 * @param {number} Y The y-coordinate of the rotation center.
+	 * @param {number} Z The z-coordinate of the rotation center.
+	 * @return {THREE.GROUP} A group that includes a box representing
+	 *                       the limb, with the specified properties.
+	 *
+	 */
+	createLimb(dx: number, dy: number, dz: number, color: any, x:number, y: number, z: number) {
+	    var limb = createGroup(x, y, z);
+	    var offset = -1 * (Math.max(dx, dz) / 2 + dy / 2);
+		var limbBox = createBox(dx, dy, dz, color, 0, offset, 0);
+		limb.add(limbBox);
+		return limb;
+	}
+	
+	/**
+	 * A method called on the character when time moves forward.
+	 */
+	update() {
+
+		// Obtain the curren time for future calculations.
+		const currentTime = (new Date().getTime()) / 1000;
+
+		// Apply actions to the character if none are currently being
+		// carried out.
+		if (!this.isJumping &&
+			!this.isSwitchingLeft &&
+			!this.isSwitchingRight &&
+			this.queuedActions.length > 0) {
+			switch(this.queuedActions.shift()) {
+				case "up":
+					this.isJumping = true;
+					this.jumpStartTime = new Date().getTime() / 1000;
+					break;
+				case "left":
+					if (this.currentLane != -1) {
+						this.isSwitchingLeft = true;
+					}
+					break;
+				case "right":
+					if (this.currentLane != 1) {
+						this.isSwitchingRight = true;
+					}
+					break;
+			}
+		}
+
+		// If the character is jumping, update the height of the character.
+		// Otherwise, the character continues running.
+		if (this.isJumping) {
+			var jumpClock = currentTime - this.jumpStartTime;
+			this.element.position.y = this.jumpHeight * Math.sin(
+				(1 / this.jumpDuration) * Math.PI * jumpClock) +
+				sinusoid(2 * this.stepFreq, 0, 20, 0,
+					this.jumpStartTime - this.runningStartTime);
+			if (jumpClock > this.jumpDuration) {
+				this.isJumping = false;
+				this.runningStartTime += this.jumpDuration;
+			}
+		} else {
+			var runningClock = currentTime - this.runningStartTime;
+			this.element.position.y = sinusoid(
+				2 * this.stepFreq, 0, 20, 0, runningClock);
+			this.head.rotation.x = sinusoid(
+				2 * this.stepFreq, -10, -5, 0, runningClock) * deg2Rad;
+			this.torso.rotation.x = sinusoid(
+				2 * this.stepFreq, -10, -5, 180, runningClock) * deg2Rad;
+			this.leftArm.rotation.x = sinusoid(
+				this.stepFreq, -70, 50, 180, runningClock) * deg2Rad;
+			this.rightArm.rotation.x = sinusoid(
+				this.stepFreq, -70, 50, 0, runningClock) * deg2Rad;
+			this.leftLowerArm.rotation.x = sinusoid(
+				this.stepFreq, 70, 140, 180, runningClock) * deg2Rad;
+			this.rightLowerArm.rotation.x = sinusoid(
+				this.stepFreq, 70, 140, 0, runningClock) * deg2Rad;
+			this.leftLeg.rotation.x = sinusoid(
+				this.stepFreq, -20, 80, 0, runningClock) * deg2Rad;
+			this.rightLeg.rotation.x = sinusoid(
+				this.stepFreq, -20, 80, 180, runningClock) * deg2Rad;
+			this.leftLowerLeg.rotation.x = sinusoid(
+				this.stepFreq, -130, 5, 240, runningClock) * deg2Rad;
+			this.rightLowerLeg.rotation.x = sinusoid(
+				this.stepFreq, -130, 5, 60, runningClock) * deg2Rad;
+
+			// If the character is not jumping, it may be switching lanes.
+			if (this.isSwitchingLeft) {
+				this.element.position.x -= 200;
+				var offset = this.currentLane * 800 - this.element.position.x;
+				if (offset > 800) {
+					this.currentLane -= 1;
+					this.element.position.x = this.currentLane * 800;
+					this.isSwitchingLeft = false;
+				}
+			}
+			if (this.isSwitchingRight) {
+				this.element.position.x += 200;
+				var offset = this.element.position.x - this.currentLane * 800;
+				if (offset > 800) {
+					this.currentLane += 1;
+					this.element.position.x = this.currentLane * 800;
+					this.isSwitchingRight = false;
+				}
+			}
+		}
+	}
+
+	/**
+	  * Handles character activity when the left key is pressed.
+	  */
+	onLeftKeyPressed() {
+		this.queuedActions.push("left");
+	}
+
+	/**
+	  * Handles character activity when the up key is pressed.
+	  */
+	onUpKeyPressed() {
+		this.queuedActions.push("up");
+	}
+
+	/**
+	  * Handles character activity when the right key is pressed.
+	  */
+	onRightKeyPressed() {
+		this.queuedActions.push("right");
+	}
+
+	/**
+	  * Handles character activity when the game is paused.
+	  */
+	onPause() {
+		this.pauseStartTime = new Date().getTime() / 1000;
+	}
+
+	/**
+	  * Handles character activity when the game is unpaused.
+	  */
+	onUnpause() {
+		var currentTime = new Date().getTime() / 1000;
+		var pauseDuration = currentTime - this.pauseStartTime;
+		this.runningStartTime += pauseDuration;
+		if (this.isJumping) {
+			this.jumpStartTime += pauseDuration;
+		}
+	}
+
+}
--- gift-secret-santa/src/world.ts
+++ gift-secret-santa/src/world.ts
@@ -1,6 +1,11 @@
 import * as THREE from 'three';
 
-class World {
+import { Character } from './character';
+import { Colors } from './constant';
+import { Tree } from './three';
+import { createBox } from './ui-utils';
+
+export class World {
     element: any;
     scene: any;
     camera: any;
@@ -14,7 +19,6 @@ class World {
     difficulty: any;
     treePresenceProb: any;
     maxTreeSize: any;
-    fogDistance: any;
     gameOver: any;
     fogDistance: number;
 
@@ -26,7 +30,10 @@ class World {
 			antialias: true
 		});
 
-        this.renderer.setSize(this.element.clientWidth, this.element.clientHeight);
+        this.renderer.setSize(
+            this.element.clientWidth, 
+            this.element.clientHeight
+        );
 		this.renderer.shadowMap.enabled = true;
 
         this.element.appendChild(this.renderer.domElement);
@@ -34,5 +41,301 @@ class World {
         this.scene = new THREE.Scene();
 		this.fogDistance = 40000;
 		this.scene.fog = new THREE.Fog(0xbadbe4, 1, this.fogDistance);
+        // Initialize the camera with field of view, aspect ratio,
+		// near plane, and far plane.
+		this.camera = new THREE.PerspectiveCamera(
+			60,
+             this.element.clientWidth / this.element.clientHeight,
+             1,
+              120000
+            );
+		this.camera.position.set(0, 1500, -2000);
+		this.camera.lookAt(new THREE.Vector3(0, 600, -5000));
+		(window as any).camera = this.camera;
+
+		// Set up resizing capabilities.
+		window.addEventListener('resize', this.handleWindowResize, false);
+
+		// Initialize the lights.
+		this.light = new THREE.HemisphereLight(0xffffff, 0xffffff, 1);
+		this.scene.add(this.light);
+
+		// Initialize the character and add it to the scene.
+		this.character = new Character();
+		this.scene.add(this.character.element);
+
+		var ground = createBox(3000, 20, 120000, Colors.sand, 0, -400, -60000);
+		this.scene.add(ground);
+
+		this.objects = [];
+		this.treePresenceProb = 0.2;
+		this.maxTreeSize = 0.5;
+		for (var i = 10; i < 40; i++) {
+			this.createRowOfTrees(i * -3000, this.treePresenceProb, 0.5, this.maxTreeSize);
+		}
+
+		// The game is paused to begin with and the game is not over.
+		this.gameOver = false;
+		this.paused = true;
+
+
+        this.keysAllowed = {};
+
+        		// Initialize the scores and difficulty.
+		this.score = 0;
+		this.difficulty = 0;
+		// document?.getElementById("score")?.innerHTML = this.score;
+
+
+
+        // document.addEventListener(
+		// 	'keydown',
+        //     onKeyDown()
+		// );
+		// document.addEventListener(
+		// 	'keyup',
+		// 	function(e) {
+		// 		this.keysAllowed[e.keyCode] = true;
+		// 	}
+		// );
+		// document.addEventListener(
+		// 	'focus',
+		// 	function(e) {
+		// 		this.keysAllowed = {};
+		// 	}
+		// );
+
+        // Begin the rendering loop.
+		this.loop();
     }
+
+    
+	/**
+	  * The main animation loop.
+	  */
+	loop() {
+
+		// Update the game.
+		if (!this.paused) {
+
+			// Add more trees and increase the difficulty.
+			if ((this.objects[this.objects.length - 1].mesh.position.z) % 3000 == 0) {
+				this.difficulty += 1;
+				var levelLength = 30;
+				if (this.difficulty % levelLength == 0) {
+					var level = this.difficulty / levelLength;
+					switch (level) {
+						case 1:
+							this.treePresenceProb = 0.35;
+							this.maxTreeSize = 0.5;
+							break;
+						case 2:
+							this.treePresenceProb = 0.35;
+							this.maxTreeSize = 0.85;
+							break;
+						case 3:
+							this.treePresenceProb = 0.5;
+							this.maxTreeSize = 0.85;
+							break;
+						case 4:
+							this.treePresenceProb = 0.5;
+							this.maxTreeSize = 1.1;
+							break;
+						case 5:
+							this.treePresenceProb = 0.5;
+							this.maxTreeSize = 1.1;
+							break;
+						case 6:
+							this.treePresenceProb = 0.55;
+							this.maxTreeSize = 1.1;
+							break;
+						default:
+							this.treePresenceProb = 0.55;
+							this.maxTreeSize = 1.25;
+					}
+				}
+				if ((this.difficulty >= 5 * levelLength && this.difficulty < 6 * levelLength)) {
+					this.fogDistance -= (25000 / levelLength);
+				} else if (this.difficulty >= 8 * levelLength && this.difficulty < 9 * levelLength) {
+					this.fogDistance -= (5000 / levelLength);
+				}
+				this.createRowOfTrees(-120000, this.treePresenceProb, 0.5, this.maxTreeSize);
+				this.scene.fog.far = this.fogDistance;
+			}
+
+			// Move the trees closer to the character.
+			this.objects.forEach(function(object) {
+				object.mesh.position.z += 100;
+			});
+
+			// Remove trees that are outside of the world.
+			this.objects = this.objects.filter(function(object) {
+				return object.mesh.position.z < 0;
+			});
+
+			// Make the character move according to the controls.
+			this.character.update();
+
+			// Check for collisions between the character and objects.
+			if (this.collisionsDetected()) {
+				this.gameOver = true;
+				this.paused = true;
+				// document.addEventListener(
+        		// 	'keydown',
+        		// 	function(e) {
+        		// 		if (e.keyCode == 40)
+            	// 		document.location.reload(true);
+        		// 	}
+    			// );
+    			var variableContent: any = document.getElementById("variable-content");
+    			variableContent.style.visibility = "visible";
+    			variableContent.innerHTML = 
+    				"Game over! Press the down arrow to try again.";
+    			var table: any = document.getElementById("ranks");
+    			var rankNames = ["Typical Engineer", "Couch Potato", "Weekend Jogger", "Daily Runner",
+    				"Local Prospect", "Regional Star", "National Champ", "Second Mo Farah"];
+    			var rankIndex: number = Math.floor(this.score / 15000);
+
+				// If applicable, display the next achievable rank.
+				if (this.score < 124000) {
+					var nextRankRow = table.insertRow(0);
+					nextRankRow.insertCell(0).innerHTML = (rankIndex <= 5)
+						? "".concat(`${(rankIndex + 1) * 15}`, "k-", `${(rankIndex + 2) * 15}`, "k")
+						: (rankIndex == 6)
+							? "105k-124k"
+							: "124k+";
+					nextRankRow.insertCell(1).innerHTML = "*Score within this range to earn the next rank*";
+				}
+
+				// Display the achieved rank.
+				var achievedRankRow = table.insertRow(0);
+				achievedRankRow.insertCell(0).innerHTML = (rankIndex <= 6)
+					? "".concat(`${(rankIndex + 1) * 15}`, "k-", `${(rankIndex + 1) * 15}`, "k").bold()
+					: (this.score < 124000)
+						? "105k-124k".bold()
+						: "124k+".bold();
+				achievedRankRow.insertCell(1).innerHTML = (rankIndex <= 6)
+					? "Congrats! You're a ".concat(rankNames[rankIndex], "!").bold()
+					: (this.score < 124000)
+						? "Congrats! You're a ".concat(rankNames[7], "!").bold()
+						: "Congrats! You exceeded the creator's high score of 123790 and beat the game!".bold();
+
+    			// Display all ranks lower than the achieved rank.
+    			if (this.score >= 120000) {
+    				rankIndex = 7;
+    			}
+    			for (var i = 0; i < rankIndex; i++) {
+    				var row = table.insertRow(i);
+    				row.insertCell(0).innerHTML = "".concat(`${i * 15}`, "k-", `${(i + 1) * 15}`, "k");
+    				row.insertCell(1).innerHTML = rankNames[i];
+    			}
+    			if (this.score > 124000) {
+    				var row = table.insertRow(7);
+    				row.insertCell(0).innerHTML = "105k-124k";
+    				row.insertCell(1).innerHTML = rankNames[7];
+    			}
+
+			}
+
+			// Update the scores.
+			this.score += 10;
+			//document.getElementById("score").innerHTML = score;
+
+		}
+
+		// Render the page and repeat.
+		this.renderer.render(this.scene, this.camera);
+		requestAnimationFrame(this.loop);
+	}
+
+	/**
+	  * A method called when window is resized.
+	  */
+	handleWindowResize() {
+		this.renderer.setSize(this.element.clientWidth, this.element.clientHeight);
+		this.camera.aspect = this.element.clientWidth / this.element.clientHeight;
+		this.camera.updateProjectionMatrix();
+	}
+
+	/**
+	 * Creates and returns a row of trees according to the specifications.
+	 *
+	 * @param {number} POSITION The z-position of the row of trees.
+ 	 * @param {number} PROBABILITY The probability that a given lane in the row
+ 	 *                             has a tree.
+ 	 * @param {number} MINSCALE The minimum size of the trees. The trees have a 
+ 	 *							uniformly distributed size from minScale to maxScale.
+ 	 * @param {number} MAXSCALE The maximum size of the trees.
+ 	 *
+	 */
+	createRowOfTrees(position, probability, minScale, maxScale) {
+		for (var lane = -1; lane < 2; lane++) {
+			var randomNumber = Math.random();
+			if (randomNumber < probability) {
+				var scale = minScale + (maxScale - minScale) * Math.random();
+				var tree = new Tree(lane * 800, -400, position, scale);
+				this.objects.push(tree);
+				this.scene.add(tree.mesh);
+			}
+		}
+	}
+
+	/**
+	 * Returns true if and only if the character is currently colliding with
+	 * an object on the map.
+	 */
+ 	collisionsDetected() {
+ 		var charMinX = this.character.element.position.x - 115;
+ 		var charMaxX = this.character.element.position.x + 115;
+ 		var charMinY = this.character.element.position.y - 310;
+ 		var charMaxY = this.character.element.position.y + 320;
+ 		var charMinZ = this.character.element.position.z - 40;
+ 		var charMaxZ = this.character.element.position.z + 40;
+ 		for (var i = 0; i < this.objects.length; i++) {
+ 			if (this.objects[i].collides(charMinX, charMaxX, charMinY, 
+ 					charMaxY, charMinZ, charMaxZ)) {
+ 				return true;
+ 			}
+ 		}
+ 		return false;
+ 	}
+
+     onKeyDown(e: any) {
+        		// Start receiving feedback from the player.
+		var left = 37;
+		var up = 38;
+		var right = 39;
+		var p = 80;
+        if (!this.gameOver) {
+            var key = e.keyCode;
+            if (this.keysAllowed[key] === false) return;
+            this.keysAllowed[key] = false;
+            if (this.paused && !this.collisionsDetected() && key > 18) {
+                this.paused = false;
+                this.character.onUnpause();
+                // document.getElementById("variable-content").style.visibility = "hidden";
+                // document.getElementById("controls").style.display = "none";
+            } else {
+                if (key == p) {
+                    this.paused = true;
+                    this.character.onPause();
+                    // document.getElementById(
+                    //     "variable-content").style.visibility = "visible";
+                    // document.getElementById(
+                    //     "variable-content").innerHTML = 
+                    //     "Game is paused. Press any key to resume.";
+                }
+                if (key == up && !this.paused) {
+                    this.character.onUpKeyPressed();
+                }
+                if (key == left && !this.paused) {
+                    this.character.onLeftKeyPressed();
+                }
+                if (key == right && !this.paused) {
+                    this.character.onRightKeyPressed();
+                }
+            }
+        }
+     }
+	
 }
